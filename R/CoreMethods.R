7a8a6cc6076de7a9a09af5578089c0d08884fd01
Tsz Hang J Lee <tl7@mib112594i.internal.sanger.ac.uk>
2019-02-20 11:40:24 +0000

    * fixed bug of 'OR' function in `hyperQueryCellTypes()` and `findCellTypes()`

--- R/CoreMethods.R
+++ R/CoreMethods.R
@@ -494,7 +494,23 @@ findCellTypes.geneList <- function(object, gene.list, datasets)
             if(!is.null(df))
             {
                 result <- as.list(setNames(as.numeric(split(df[,2], seq(nrow(df)))), df[,1]))
-                if(length(unique(df[,1])) == nrow(df)) return(result) else return(unstack(stack(result)))
+                if(length(unique(df[,1])) == nrow(df)) 
+                {
+                    return(result)
+                } 
+                else 
+                {
+                    if(length(unique(names(result))) == 1) 
+                    {
+                        tmp <-list(stack(result)$values)
+                        names(tmp) <- unique(names(result))
+                        return(tmp)
+                    }  
+                    else 
+                    {
+                        return(unstack(stack(result)))
+                    }
+                }
             }
             else
             {
